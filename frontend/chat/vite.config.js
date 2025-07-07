f3216468d8366ed91804aaeb9213966c7df44718
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-07 16:55:10 +0500

    update the chatapp packeges

--- frontend/chat/vite.config.js
+++ frontend/chat/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // ensure Vercel uses the correct build output
-  },
-  server: {
-    port: 5173,  // optional: override default port for local dev
-    open: true   // optional: auto-open browser
-  }
 })
