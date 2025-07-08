4e5936c70e743308f9842561f5021ba9d2063463
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-08 13:54:41 +0500

    fix: Updatd the domain link in frontend works prperly!

--- frontend/chat/vite.config.js
+++ frontend/chat/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vite.dev/config/ksdsld
+// This is a Vite configuration file for a React project.
 export default defineConfig({
   plugins: [react()],
   base: '/',
