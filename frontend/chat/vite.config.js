65fc69ae3c9fbb083a4b924cbf3c6e84f60e67e6
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-08 13:57:06 +0500

    now

--- frontend/chat/vite.config.js
+++ frontend/chat/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/ksdsld
 // This is a Vite configuration file for a React project.
+// It sets up the project to use React with SWC for faster builds and includes a base
 export default defineConfig({
   plugins: [react()],
   base: '/',
